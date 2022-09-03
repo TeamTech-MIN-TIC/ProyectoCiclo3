3f1ea33f6f60b74ec3c312866ca141f87f9d1dc1
José Dario Soto <sotomark2021@gmail.com>
2022-09-03 16:03:22 -0500

    Primer royecto en la Web

---
+++ src/main/java/com/ProyectoCiclo3/proyectociclo3/ControladorGeneral.java
@@ -0,0 +1,13 @@
+package com.ProyectoCiclo3.proyectociclo3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorGeneral {
+
+    @GetMapping
+    public String saludo() {
+        return "Este es m primer Proyecto en la Web";
+    }
+}
