8bd9e5508b12322e649814d017b89a0bff139fb2
José Dario Soto <sotomark2021@gmail.com>
2022-09-03 16:05:42 -0500

    Primer royecto en la Web

--- src/main/java/com/ProyectoCiclo3/proyectociclo3/controller/ControladorGeneral.java
+++ src/main/java/com/ProyectoCiclo3/proyectociclo3/controller/ControladorGeneral.java
@@ -1,4 +1,4 @@
-package com.ProyectoCiclo3.proyectociclo3;
+package com.ProyectoCiclo3.proyectociclo3.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
