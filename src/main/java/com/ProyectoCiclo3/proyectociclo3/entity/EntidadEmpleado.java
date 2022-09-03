7dc76de365c08e935f47c3c71793ee78d3fe8607
José Dario Soto <sotomark2021@gmail.com>
2022-09-03 17:23:06 -0500

    Organizar Archivo Empleados

---
+++ src/main/java/com/ProyectoCiclo3/proyectociclo3/entity/EntidadEmpleado.java
@@ -0,0 +1,52 @@
+package com.ProyectoCiclo3.proyectociclo3.entity;
+
+public class EntidadEmpleado {
+
+    private String nombre;
+    private String correo;
+    private String empresa;
+    private String rol;
+
+    public EntidadEmpleado(String nombre, String correo, String empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public EntidadEmpleado() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
