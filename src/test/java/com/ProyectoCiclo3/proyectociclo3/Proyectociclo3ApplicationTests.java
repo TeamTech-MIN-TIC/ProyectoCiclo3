e4e62afc9109bdd71c0f4ffe015260cd7e2ab61c
José Dario Soto <sotomark2021@gmail.com>
2022-09-03 15:53:31 -0500

    Subiendo Proyecto

---
+++ src/test/java/com/ProyectoCiclo3/proyectociclo3/Proyectociclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoCiclo3.proyectociclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectociclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
